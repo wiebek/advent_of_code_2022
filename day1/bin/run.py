138216fcda4260f64878aed45029494bea82cc04
Wiebe <wiebe.kooistra@gmail.com>
2022-12-01 11:59:29 +0100

    adding day 1
    Did this myself

---
+++ day1/bin/run.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import sys
+
+
+def main(file_):
+    """
+    Find the elf with the most calories
+    """
+    d = dict()
+    elf = 1
+    cal = 0
+    with open(file_) as f:
+        for line in f:
+            line = line.strip()
+            if line == "":
+                d[elf] = cal
+                elf += 1
+                cal = 0
+            if line != "":
+                cal += int(line)
+    d[elf] = cal
+    elf += 1
+
+    print(max(d.values()))
+    print(sum(sorted(d.values(), reverse=True)[:3]))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
